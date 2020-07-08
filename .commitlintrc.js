f78ddbba5f3ae538868df53b09456cacd1cba7b8
hejiahuan@zcckj.com <1042087441@qq.com>
2020-07-08 23:04:29 +0800

    fix: 修改配置文件

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,21 +2,21 @@ module.exports = {
   "extends": [
     "@commitlint/config-conventional"
   ],
+  "type-enum": [
+    "build", // 构造工具的或者外部依赖的改动，例如webpack，npm
+    "ci", // 与CI（持续集成服务）有关的改动
+    "chore", // 构建过程或者辅助工具的改变
+    "docs", // 文档
+    "feat", // 新功能
+    "fix", // 修复bug
+    "perf", // 提高性能的改动
+    "refactor", // 重构
+    "revert", // 执行git revert
+    "style", // 格式
+    "test" // 测试
+  ],
   "rules": {
-    "type-enum": [
-      "build", // 构造工具的或者外部依赖的改动，例如webpack，npm
-      "ci", // 与CI（持续集成服务）有关的改动
-      "chore", // 构建过程或者辅助工具的改变
-      "docs", // 文档
-      "feat", // 新功能
-      "fix", // 修复bug
-      "perf", // 提高性能的改动
-      "refactor", // 重构
-      "revert", // 执行git revert
-      "style", // 格式
-      "test" // 测试
-    ],
     "body-leading-blank": [1, "always"],
-    "body-max-line-length": [2, "always", 400]
+    "body-max-line-length": [2, "always", 100]
   }
 };
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'src/**/*.{ts, tsx}': () => 'tsc -p . noEmit',
   'src/**/*.{css, less, sass}': ['stylelint --fix'],
-  'src/**/*.{js, jsx, ts, tsx}': ['prettier --write', 'eslint --fix', 'git add'],
+  'src/**/*.{js, jsx, ts, tsx}': ['prettier --write', 'eslint --fix'],
 };
